f4fd64328c7e10890c1a6f89a892e2a0b5574460
Alex Carrozzi <ack444@wildcats.unh.edu>
2015-02-03 22:20:30 -0500

    Setting Up Initial OAuth

--- simple.php
+++ simple.php
@@ -1,67 +1,27 @@
 <?php
-error_reporting(E_ALL);
-require_once 'google-api-php-client/src/Google_Client.php';
-require_once 'google-api-php-client/src/contrib/Google_CalendarService.php';
-session_start();
-
-if ((isset($_SESSION)) && (!empty($_SESSION))) {
-   echo "There are cookies<br>";
-   echo "<pre>";
-   print_r($_SESSION);
-   echo "</pre>";
-}
+// If you've used composer to include the library, remove the following line
+// and make sure to follow the standard composer autoloading.
+// https://getcomposer.org/doc/01-basic-usage.md#autoloading
+require_once 'google-api-php-client/autoload.php';
 
 $client = new Google_Client();
-$client->setApplicationName("Google Calendar PHP Starter Application");
-$client->setClientId('###');
-$client->setClientSecret('###');
-$client->setRedirectUri('http://###/index.php');
-$client->setDeveloperKey('###');
-$cal = new Google_CalendarService($client);
+// OAuth2 client ID and secret can be found in the Google Developers Console.
+$client->setClientId('191668664245-2elcebkqrt3bve8eoj0jq7vfqn1istkt.apps.googleusercontent.com');
+$client->setClientSecret('2jGQmUbYVHbZyAeS-l0gbb10');
+$client->setRedirectUri('https://127.0.0.1/oauth2callback');
+$client->addScope('https://www.googleapis.com/auth/calendar');
 
-if (isset($_GET['logout'])) {
-  echo "<br><br><font size=+2>Logging out</font>";
-  unset($_SESSION['token']);
-}
+$service = new Google_Service_Calendar($client);
 
-if (isset($_GET['code'])) {
-  echo "<br>I got a code from Google = ".$_GET['code']; // You won't see this if redirected later
-  $client->authenticate($_GET['code']);
-  $_SESSION['token'] = $client->getAccessToken();
-  header('Location: http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']);
-  echo "<br>I got the token = ".$_SESSION['token']; // <-- not needed to get here unless location uncommented
-}
+$authUrl = $client->createAuthUrl();
 
-if (isset($_SESSION['token'])) {
-  echo "<br>Getting access";
-  $client->setAccessToken($_SESSION['token']);
-}
+//Request authorization
+echo "Please visit:\n$authUrl\n\n";
+echo "Please enter the auth code:\n";
+$authCode = trim(fgets(STDIN));
 
-if ($client->getAccessToken()){
-
-  echo "<hr><font size=+1>I have access to your calendar</font>";
-  $event = new Google_Event();
-  $event->setSummary('Halloween');
-  $event->setLocation('The Neighbourhood');
-  $start = new Google_EventDateTime();
-  $start->setDateTime('2013-9-29T10:00:00.000-05:00');
-  $event->setStart($start);
-  $end = new Google_EventDateTime();
-  $end->setDateTime('2013-9-29T10:25:00.000-05:00');
-  $event->setEnd($end);
-  $createdEvent = $cal->events->insert('###', $event);
-  echo "<br><font size=+1>Event created</font>";
-
-  echo "<hr><br><font size=+1>Already connected</font> (No need to login)";
-
-} else {
-
-  $authUrl = $client->createAuthUrl();
-  print "<hr><br><font size=+2><a href='$authUrl'>Connect Me!</a></font>";
-
-}
-
-$url = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
-echo "<br><br><font size=+2><a href=$url?logout>Logout</a></font>";
+// Exchange authorization code for access token
+$accessToken = $client->authenticate($authCode);
+$client->setAccessToken($accessToken);
 
 ?>
