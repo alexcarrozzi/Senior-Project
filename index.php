b59d33ef2f2297c7bc2b783b205eedb036e9f69c
Alex Carrozzi <ack444@wildcats.unh.edu>
2015-02-11 13:06:58 -0500

    testing

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+$type = 2;
+
 ini_set('display_errors', 1);
 session_start();
 //unset($_SESSION);
@@ -6,10 +8,12 @@ session_start();
 require_once 'google-api-php-client/src/Google/Client.php';
 require_once 'google-api-php-client/src/Google/Service/Calendar.php';
 
-
 const CLIENT_ID = '191668664245-h1t5dbipvmglh09mc27bo3ckdfjjojqk.apps.googleusercontent.com';
 const SERVICE_ACCOUNT_NAME = '191668664245-h1t5dbipvmglh09mc27bo3ckdfjjojqk@developer.gserviceaccount.com';
 const KEY_FILE = 'ScheduleIt-2b0035283339.p12';
+
+if($type==1){
+//SERVICE SET UP
 
 $client = new Google_Client();
 $client->setClientId(CLIENT_ID);
@@ -24,30 +28,6 @@ if (isset($_SESSION['token'])) {
 
 $key = file_get_contents(KEY_FILE);
 
-if (isset($_GET['code'])){
-    $resp = $client->authenticate($_GET['code']);
-    $_SESSION['upload_token'] = $client->getAccessToken();
-    $array = get_object_vars(json_decode($resp));
-    $refreshToken = $array['refreshToken'];
-    if($client->isAccessTokenExpired()){
-        $client->refreshToken($refreshToken);
-    }
-    $redirect = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
-    header('Location: ' . filter_var($redirect, FILTER_SANITIZE_URL));
-}
-
-
-if (isset($_SESSION['upload_token']) && $_SESSION['upload_token']){
-  $client->setAccessToken($_SESSION['upload_token']);
-  if ($client->isAccessTokenExpired()){
-    $client->refreshToken($refreshToken);
-    unset($_SESSION['upload_token']);
-  }
-}else{
-  $authUrl = $client->createAuthUrl();
-}
-
-
 if (isset($_SESSION['token'])){
     $client->setAccessToken($_SESSION['token']);
     //echo $_SESSION['token']; 
@@ -60,7 +40,7 @@ if (isset($_SESSION['token'])){
         'http://oauth.net/grant_type/jwt/1.0/bearer')
     );  
 }
-
+//EXECUTION
 try{
     $service = new Google_Service_Calendar($client);
     $calendarList = $service->calendarList->listCalendarList();
@@ -80,9 +60,79 @@ try{
         break;
       }
     }
-        
-    
 }catch(Exception $e){
     echo $e->getMessage();
+}
+//END SERVICE SET UP
+
+}else{
+
+
+//OTHER SET UP
+require_once 'google-api-php-client/autoload.php';
+
+$client_id = '191668664245-216ovjoman2vfe5g7e6fepnsscp0bj4q.apps.googleusercontent.com';
+$client_secret = 'J6iNZeLn_iRxxYv1B07jCeTa';
+$redirect_uri = 'http://localhost/Senior-Project/';
+$client2 = new Google_Client();
+$client2->setClientId($client_id);
+$client2->setClientSecret($client_secret);
+$client2->setRedirectUri($redirect_uri);
+$client2->setScopes('https://www.googleapis.com/auth/calendar');
+
+if (isset($_REQUEST['logout'])) {
+  unset($_SESSION['access_token']);
+}
+
+if (isset($_GET['code'])) {
+  $client2->authenticate($_GET['code']);
+  $_SESSION['access_token'] = $client2->getAccessToken();
+  $redirect2 = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+  header('Location: ' . filter_var($redirect2, FILTER_SANITIZE_URL));
+}
+if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+  $client2->setAccessToken($_SESSION['access_token']);
+} else {
+  $authUrl2 = $client2->createAuthUrl();
+}
+
+if ($client2->getAccessToken()) {
+  $_SESSION['access_token'] = $client2->getAccessToken();
+  $token_data = $client2->verifyIdToken()->getAttributes();
+}
+?>
+<div class="box">
+  <div class="request">
+<?php
+if (isset($authUrl2)) {
+  echo "<a class='login' href='" . $authUrl2 . "'>Connect Me!</a>";
+} else {
+  echo "<a class='logout' href='?logout'>Logout</a>";
+}
+
+//EXECUTION
+try{
+    $service = new Google_Service_Calendar($client2);
+    $calendarList = $service->calendarList->listCalendarList();
+    echo "<pre>";
+    print_r($calendarList);
+    echo "</pre>";
+    
+    while(true) {
+      foreach ($calendarList->getItems() as $calendarListEntry) {
+        echo $calendarListEntry->getSummary();
+      }
+      $pageToken = $calendarList->getNextPageToken();
+      if ($pageToken) {
+        $optParams = array('pageToken' => $pageToken);
+        $calendarList = $service->calendarList->listCalendarList($optParams);
+      } else {
+        break;
+      }
+    }
+}catch(Exception $e){
+    echo $e->getMessage();
+}
+//END OTHER
 }
 ?>
