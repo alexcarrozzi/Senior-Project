c72ce9ff40fdf6385200561633ea52cd44869b5e
Alex Carrozzi <ack444@wildcats.unh.edu>
2015-03-11 13:46:21 -0400

    changed mail service to google

--- utilities/email.php
+++ utilities/email.php
@@ -1,4 +1,7 @@
 <?php
+    // Pear Mail Library
+    require_once "Mail.php";
+
     class Email{
         private $_from;
         
@@ -6,19 +9,49 @@
             $this->_from = $from;
         }
         
-        public function send($to,$subject,$msg){
-            if(strlen($to)==0){
+        public function send($argto,$argsubject,$msg){
+            if(strlen($argto)==0){
                 throw new Exception("'To' field cannot be empty!");
             }
             if(strlen($msg)==0){
                 throw new Exception("'Message' field cannot be empty!");
             }
-            $mailto = $to;
             $body = wordwrap($msg,70);
-            $headers = "From: ".$this->_from." \r\n".
+            $headers = "From: ".$this->_from."\r\n".
                         "CC: [Advisor Email]";
             
-            return mail($mailto,$subject,$body,$headers);
+            $from = '<senior.project705@gmail.com>';
+            $to = "<$argto>";
+            $subject = $argsubject;
+            $body = "$body";
+            
+            $headers = array(
+            'From' => $from,
+            'To' => $to,
+            'Subject' => $subject
+            );
+              
+              
+              $smtp = Mail::factory('smtp', array(
+                    'host' => 'ssl://smtp.gmail.com',
+                    'port' => '465',
+                    'auth' => true,
+                    'username' => 'senior.project705@gmail.com',
+                    'password' => 'wildcats007'
+                ));
+
+            $mail = $smtp->send($to, $headers, $body);
+
+            if (PEAR::isError($mail)) {
+                echo('<p>' . $mail->getMessage() . '</p>');
+                return false;
+            } else {
+                echo('<p>Message successfully sent!</p>');
+                return true;
+            }      
+            
+          
+            //return mail($mailto,$subject,$body,$headers);
         }
     }
     
