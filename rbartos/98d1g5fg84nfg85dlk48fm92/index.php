b2018dce19c7ee10d7caef67424e410b671bd7fa
Alex Carrozzi <ack444@wildcats.unh.edu>
2015-03-26 17:47:37 -0400

    testing oauth

--- rbartos/98d1g5fg84nfg85dlk48fm92/index.php
+++ rbartos/98d1g5fg84nfg85dlk48fm92/index.php
@@ -1,36 +1,98 @@
 <?php
-	session_start();
-	require_once '../../utilities/common.php';
-    
-	require_once '../../google-api-php-client/autoload.php';
-	$client = new Google_Client();
-	$client->setAccessType('online'); // default: offline
-	$client->setApplicationName('ScheduleIt');
-	$client->setClientId('191668664245-k6apjlo3hojik7rphq9aet58hiu4pc26.apps.googleusercontent.com');
-	$client->setClientSecret('t86-1-Msaw9C7wuPKZ-dvLYK');
-	$client->setRedirectUri('http://scheduleit.cs.unh.edu:8080/rbartos/98d1g5fg84nfg85dlk48fm92/');
-	$client->setDeveloperKey('AIzaSyDzsF1TFKgiX1YVx7oBdmorGrkwIiFah88'); // API key
-	$client->setScopes('https://www.googleapis.com/auth/userinfo.profile'); 
-
-	if (isset($_GET['logout'])) { // logout: destroy token
-		unset($_SESSION['token']);
-		die('Logged out.');
-	}
-	
-	if (isset($_GET['code'])) { // we received the positive auth callback, get the token and store it in session
-		$client->authenticate($_GET['code']);
-		$_SESSION['token'] = $client->getAccessToken();
-	}
-	
-	if (isset($_SESSION['token'])) { // extract token from session and configure client
-		$token = $_SESSION['token'];
-		$client->setAccessToken($token);
-	}
-	
-	if (!$client->getAccessToken()) { // auth call to google
-		$authUrl = $client->createAuthUrl();
-		header("Location: ".$authUrl);
-		die;
-	}
-	echo 'Hello, world.';
+/*
+ * Copyright 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+require_once '../../google-api-php-client/src/Google/Client.php';
+require_once '../../google-api-php-client/src/Google/Service/Plus.php';
+session_start();
+$client = new Google_Client();
+$client->setAccessType('online'); // default: offline
+$client->setApplicationName('ScheduleIt');
+$client->setClientId('191668664245-k6apjlo3hojik7rphq9aet58hiu4pc26.apps.googleusercontent.com');
+$client->setClientSecret('t86-1-Msaw9C7wuPKZ-dvLYK');
+$client->setRedirectUri('http://scheduleit.cs.unh.edu:8080/');
+$client->setDeveloperKey('AIzaSyDzsF1TFKgiX1YVx7oBdmorGrkwIiFah88'); // API key
+$client->setScopes('https://www.googleapis.com/auth/userinfo.profile'); 
+$client->setScopes(array('https://www.googleapis.com/auth/plus.me'));
+$plus = new Google_Service_Plus($client);
+if (isset($_REQUEST['logout'])) {
+  unset($_SESSION['access_token']);
+}
+if (isset($_GET['code'])) {
+  $client->authenticate();
+  $_SESSION['access_token'] = $client->getAccessToken();
+  header('Location: http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']);
+}
+if (isset($_SESSION['access_token'])) {
+  $client->setAccessToken($_SESSION['access_token']);
+}
+if ($client->getAccessToken()) {
+  $me = $plus->people->get('me');
+  // These fields are currently filtered through the PHP sanitize filters.
+  // See http://www.php.net/manual/en/filter.filters.sanitize.php
+  $url = filter_var($me['url'], FILTER_VALIDATE_URL);
+  $img = filter_var($me['image']['url'], FILTER_VALIDATE_URL);
+  $name = filter_var($me['displayName'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+  $personMarkup = "<a rel='me' href='$url'>$name</a><div><img src='$img'></div>";
+  $optParams = array('maxResults' => 100);
+  $activities = $plus->activities->listActivities('me', 'public', $optParams);
+  $activityMarkup = '';
+  foreach($activities['items'] as $activity) {
+    // These fields are currently filtered through the PHP sanitize filters.
+    // See http://www.php.net/manual/en/filter.filters.sanitize.php
+    $url = filter_var($activity['url'], FILTER_VALIDATE_URL);
+    $title = filter_var($activity['title'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
+    $content = filter_var($activity['object']['content'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
+    $activityMarkup .= "<div class='activity'><a href='$url'>$title</a><div>$content</div></div>";
+  }
+  // The access token may have been updated lazily.
+  $_SESSION['access_token'] = $client->getAccessToken();
+} else {
+  $authUrl = $client->createAuthUrl();
+}
 ?>
+<!doctype html>
+<html>
+<head><link rel='stylesheet' href='style.css' /></head>
+<body>
+<header><h1>Google+ Sample App</h1></header>
+<div class="box">
+
+<?php if(isset($personMarkup)): ?>
+<div class="me"><?php print $personMarkup ?></div>
+<?php
+$me = $plus->people->get('me');
+print "ID: {$me['id']}\n";
+print "Display Name: {$me['displayName']}\n";
+print "Image Url: {$me['image']['url']}\n";
+print "Url: {$me['url']}\n";
+
+?>
+<?php endif ?>
+
+<?php if(isset($activityMarkup)): ?>
+<div class="activities">Your Activities: <?php print $activityMarkup ?></div>
+<?php endif ?>
+
+<?php
+  if(isset($authUrl)) {
+    print "<a class='login' href='$authUrl'>Connect Me!</a>";
+  } else {
+   print "<a class='logout' href='?logout'>Logout</a>";
+  }
+?>
+</div>
+</body>
+</html>
