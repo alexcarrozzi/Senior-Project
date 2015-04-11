5ac2ed0f369717110ff04c04e68dc2dd94e6fec6
Alex Carrozzi <ack444@wildcats.unh.edu>
2015-04-11 14:48:06 -0400

    testing cancellation redirection page

--- utilities/after_cancel.php
+++ utilities/after_cancel.php
@@ -1,3 +1,3 @@
 <p>You have successfully cancelled your meeting on <?= date('l, F j, Y \a\t g:i',$canceled_time) ?><br/> 
-    If you wish to sign&ndash;up for a new time, please go to <a href="http://scheduleit.cs.unh.edu:8080/?cid=<?= $base64encode($cal_id) ?>">http://scheduleit.cs.unh.edu:8080/?cid=<?= $base64encode($cal_id)</a>
+    If you wish to sign&ndash;up for a new time, please go to <a href="http://scheduleit.cs.unh.edu:8080/?cid=<?= base64encode($cal_id) ?>">http://scheduleit.cs.unh.edu:8080/?cid=<?= base64encode($cal_id)</a>
 </p>
