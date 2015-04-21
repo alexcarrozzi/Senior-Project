e9b9d0484ed120732aaa61f2bed7c2da92b0df33
Alex Carrozzi <ack444@wildcats.unh.edu>
2015-04-21 15:09:29 -0400

    tweaking calcellation display time

--- utilities/after_cancel.php
+++ utilities/after_cancel.php
@@ -9,6 +9,6 @@
  * 
  */
 ?>
-<p>You have successfully cancelled your meeting on <?= date('l, F j, Y \a\t g:i a',$canceled_time) ?><br/> 
+<p>You have successfully cancelled your meeting on <?= date('l, F j, Y \a\t g:i a',fmt_gdate($canceled_time)) ?><br/> 
     If you wish to sign&ndash;up for a new time, please go to <a href="http://scheduleit.cs.unh.edu:8080/?cid=<?= base64_encode($cal_id) ?>">http://scheduleit.cs.unh.edu:8080/?cid=<?= base64_encode($cal_id)?></a>
 </p>
